d76deb72800e763f1d2c6ab42649eed5399160e7
zchroma <222100474+zchroma@users.noreply.github.com>
2025-11-02 20:53:37 +0200

    Chore: Update requirements.txt

--- stcats_cs/settings.py
+++ stcats_cs/settings.py
@@ -41,6 +41,7 @@ TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -63,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
